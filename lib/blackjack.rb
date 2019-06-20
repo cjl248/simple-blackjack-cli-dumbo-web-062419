2339a00f4bcaa544bccb069eac5434172d705abd
Chris Liendo <cjl248@cornell.edu>
2019-06-20 10:37:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,43 +1,64 @@
+# welcome function
 def welcome
   # code #welcome here
+  
 end
 
+
+# deal_card function
 def deal_card
   # code #deal_card here
 end
 
+
+# display_card_total function
 def display_card_total
   # code #display_card_total here
 end
 
+
+# prompt_user function
 def prompt_user
   # code #prompt_user here
 end
 
+
+# get_user_input function
 def get_user_input
   # code #get_user_input here
 end
 
+
+# end_game function
 def end_game
   # code #end_game here
 end
 
+
+# initial_round function
 def initial_round
   # code #initial_round here
 end
 
+
+# hit? function
 def hit?
   # code hit? here
 end
 
+
+# invalid_command function
 def invalid_command
   # code invalid_command here
 end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
+
+# runner function
 def runner
   # code runner here
 end
